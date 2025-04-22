b225504b24e2e7a9cd40efc2b28f935d909f198e
rezakurniasetiawan <56224328+rezakurniasetiawan@users.noreply.github.com>
2025-04-22 17:15:13 +0700

    dsfdsfsdf

--- app/Filament/Resources/GamePlay1Resource/Pages/EditGamePlay1.php
+++ app/Filament/Resources/GamePlay1Resource/Pages/EditGamePlay1.php
@@ -13,7 +13,7 @@ class EditGamePlay1 extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
     }
 }
--- app/Filament/Resources/GamePlay2Resource/Pages/EditGamePlay2.php
+++ app/Filament/Resources/GamePlay2Resource/Pages/EditGamePlay2.php
@@ -13,7 +13,7 @@ class EditGamePlay2 extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
     }
 }
